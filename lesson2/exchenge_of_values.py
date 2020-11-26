10b5c127bcd107b7c72535c94cbd2ea0aa430639
TrueLemmus <truelemmus@gmail.com>
2020-11-26 22:44:05 +0300

    lesson2 directory remover

--- lesson2/exchenge_of_values.py
+++
@@ -1,7 +0,0 @@
-random_list = input("Введите несколько элементов списка разделённых запятой:").split(", ")
-print(f'Ваш список: {random_list}')
-counter = 0
-for index in range(int(len(random_list) / 2)):
-    random_list[counter], random_list[counter+1] = random_list[counter+1], random_list[counter]
-    counter += 2
-print(f'Ваш список в которм соседние элементы заменены местами: {random_list}')
--- lesson2/list.py
+++
@@ -1,3 +0,0 @@
-random_list = ['some text', 123, 3.14, bool(0)]
-for element in random_list:
-    print(type(element))
